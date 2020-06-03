4b66260b7e93d51080959502464a2759ec5aaf02
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-06-03 18:16:39 +0200

    improved isPrime function

--- functions.py
+++ functions.py
@@ -1,6 +1,21 @@
+import math
+
 def isPrime(n):
-    for i in range(3,int(n/2)+1,2):
-        if n%i==0:
+    """Returns True if 'n' is a prime number, returns False othervise"""
+
+    if n == 1:
+        return False # 1 is not a prime
+    
+    if n == 2:
+        return True
+
+    # if n is even and not 2, its not a prime number
+    if n > 2 and n % 2 == 0:
+        return False
+    
+    max_divisor = math.floor(math.sqrt(n))
+    for i in range(3, 1 + max_divisor ,2):
+        if n % i == 0:
             return False
     
     return True
